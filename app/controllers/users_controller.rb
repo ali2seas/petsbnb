7959d58a139c04c5894683a3314401cfea5502c5
Ania Czekaj <Akczekaj@gmail.com>
2018-08-01 18:02:30 +0800

    user controller created

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    if @user.update(users_params)
+      redirect_to dashboard_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  private
+  def users_params
+    params.require(:user).permit(:first_name, :last_name, :description, :age, :photo)
+  end
+end
