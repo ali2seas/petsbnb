d9262fd488ce24a857db985cd4091bad44823084
Lena Plotnikova <plotman@gmail.com>
2018-08-02 13:12:49 +0800

    Added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+15.times do
+user = User.new(
+  first_name: Faker::Name.first_name,
+  last_name: Faker::Name.last_name,
+  email: Faker::Internet.email,
+  password: 'topsecret',
+  password_confirmation: 'topsecret'
+  )
+  user.save!
+
+    pet = Pet.new(
+    name: Faker::Dog.name,
+    address: Faker::Address.full_address,
+    category: Faker::Dog.breed,
+    age: Faker::Number.number(1),
+    size: Faker::Dog.size,
+    user_id: User.last.id
+    )
+    pet.save!
+
+      booking = Booking.new(
+      user_id: User.last.id,
+      pet_id: Pet.last.id,
+      start_date: Date.today,
+      end_date: Date.today+3.day
+      )
+      booking.save!
+
+end
+puts 'Finished!'
+
+
+
