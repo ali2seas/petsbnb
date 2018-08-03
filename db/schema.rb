70b93221d45f09a2acc7b1b923fa446e3c90767f
Ania Czekaj <Akczekaj@gmail.com>
2018-08-03 15:18:17 +0800

    home page dispalying our dogs

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2018_08_03_043133) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,9 +36,9 @@ ActiveRecord::Schema.define(version: 2018_08_03_043133) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
     t.float "latitude"
     t.float "longitude"
-    t.string "photo"
     t.index ["user_id"], name: "index_pets_on_user_id"
   end
 
